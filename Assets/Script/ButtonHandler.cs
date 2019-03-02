85b3fa6db82f6d626779b76acddcab85eb94f477
tampoine <antoine.arseneault@hotmail.com>
2019-03-01 22:14:33 -0500

    Buttonhandler/gameManager et UI

---
+++ Assets/Script/ButtonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+
+
+    public void Click_StatsMenu()
+    {
+
+        if(this.gameObject.activeSelf == true)
+            this.gameObject.SetActive(false);
+        else
+            this.gameObject.SetActive(true);
+
+    }
+
+}
---
+++ Assets/Script/GameMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField]
+    public Text week, intel, cardio, strenght, dance;
+
+    public int Week, Intel, Cardio, Strenght, Dance;
+
+    void Start()
+    {
+        Week = 2;
+        week.text = Week.ToString();
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
