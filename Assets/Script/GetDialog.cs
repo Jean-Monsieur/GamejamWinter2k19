d06b6b1cbfe64a87a1e8295fbf6673c5bcc77858
Yoan Villeneuve <yo10_snow@hotmail.com>
2019-03-01 20:15:14 -0500

    Ajout script + dossier Resources > Dialog

--- Assets/Script/GetDialog.cs
+++ Assets/Script/GetDialog.cs
@@ -1,13 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GetDialog : MonoBehaviour
 {
+
+    public string Nom;
+    public string TypeDialog;
+    private string Dialog;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        try
+        {
+            //Load a text file (Assets/Resources/Text/textFile01.txt)
+            var textFile = Resources.Load<TextAsset>("Dialog/"+Nom+"/"+TypeDialog);
+            if (textFile == null)
+            {
+                Debug.LogError("Fichier text inexistant");
+                throw new InvalidOperationException("Fichier inexistant");
+            }
+            else
+                Dialog = textFile.text;
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.StackTrace.ToString());
+            Debug.LogError("Erreur avec le fichier");
+            this.enabled = false;
+        }
+    }
+
+    public String getDialog()
+    {
+        return Dialog;
     }
 
     // Update is called once per frame
